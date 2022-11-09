81e2564f7778e0fcceb365cccfeb70975638104e
davimattostw <davi.mattos.tw@naturapay.net>
2022-11-09 13:44:47 -0300

    refactor: add index files with multiple imports

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 
-import Header from '@/presentation/components/login-header/login-header'
-import Footer from '@/presentation/components/footer/footer'
-import Input from '@/presentation/components/input/input'
-import FormStatus from '@/presentation/components/form-status/form-status'
+import { Header, Footer, Input, FormStatus } from '@/presentation/components'
 
 import Styles from './login-styles.scss'
 
