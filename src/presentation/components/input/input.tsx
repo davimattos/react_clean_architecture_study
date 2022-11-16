86a5f857762494afc05fadc46657e6f681c27c00
davimattostw <davi.mattos.tw@naturapay.net>
2022-11-16 15:46:19 -0300

    feat: ensure login start with inputs with required state

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Context from '@/presentation/contexts/form/form-content'
 
 import Styles from './input-styles.scss'
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
+  const { errorState } = useContext(Context)
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false
+  }
+
+  const getTitle = (): string => {
+    return errorState[props.name]
+  }
+
+  const getStatus = (): string => {
+    return Styles.badge
   }
 
   return (
     <div className={Styles.inputWrap}>
       <input {...props} readOnly onFocus={enableInput}/>
       <span className={Styles.status}>
-        <span className={Styles.badge} />
+        <span data-testid={`${props.name}-status`} title={getTitle()} className={getStatus()}/>
       </span>
     </div>
   )
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -5,25 +5,25 @@ import Context from '@/presentation/contexts/form/form-content'
 
 import Styles from './login-styles.scss'
 
-type StateProps = {
-  isLoading: boolean
-  errorMessage: string
-}
-
 const Login: React.FC = () => {
-  const [state] = useState<StateProps>({
-    isLoading: false,
-    errorMessage: ''
+  const [state] = useState({
+    isLoading: false
+  })
+
+  const [errorState] = useState({
+    email: 'Campo obrigatório',
+    password: 'Campo obrigatório',
+    main: ''
   })
 
   return (
     <div className={Styles.login}>
       <Header />
-      <Context.Provider value={state}>
+      <Context.Provider value={{ state, errorState }}>
         <form className={Styles.form}>
           <h2>Login</h2>
-          <Input type="email" name="email" placeholder="Digite seu e-mail" />
-          <Input type="password" name="password" placeholder="Digite sua senha" />
+          <Input aria-label='email' type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input aria-label='password' type="password" name="password" placeholder="Digite sua senha" />
           <button className={Styles.submit} disabled type="submit">Entrar</button>
           <a href="" className={Styles.link}>Criar conta</a>
           <FormStatus />
